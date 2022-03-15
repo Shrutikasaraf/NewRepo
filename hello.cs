24c49cd1ba4971a33c9b134e523fd24a43af2ab3
Shrutikasaraf <101540669+Shrutikasaraf@users.noreply.github.com>
2022-03-15 10:02:05 +0530

    1

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
